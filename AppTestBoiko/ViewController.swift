3f3b7c817e85f5bf01dfea1fa77991df042e79a2
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2020-11-01 17:04:37 +0200

    Initial Commit

---
+++ AppTestBoiko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTestBoiko
+//
+//  Created by Данило Бойко on 01.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
