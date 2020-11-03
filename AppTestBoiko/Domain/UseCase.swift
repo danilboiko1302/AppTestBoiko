c8b3308790ac268a5d3ead6b78a646be448635cc
danilboiko1302 <51995092+danilboiko1302@users.noreply.github.com>
2020-11-03 11:52:10 +0200

    UseCAse Updated

--- AppTestBoiko/Domain/UseCase.swift
+++ AppTestBoiko/Domain/UseCase.swift
@@ -9,15 +9,34 @@ import Foundation
 class UseCase{
     static var  data:Post = Post()
     
+    static var  image:Data = Data()
+    
+    
+    static func saved(){
+        Repository.updateSavedPost()
+        
+    }
     
     static func requestForUpdate (){
-        print("I am UseCase, I need to get new Information from Repository.")
+       // print("I am UseCase, I need to get new Information from Repository.")
         Repository.requestForUpdate()
     }
-   
     
-    static func update(data: PostStr?){
-        print("I am UseCase, I got new information from Repository, I will process it and update View")
+    static func requestForImage (){
+       // print("Repository.requestForUpdate()")
+        Repository.requestForImage()
+    }
+    static func updateImage(image: Data?){
+        if let data = image{
+            self.image = data
+            NotificationCenter.default.post(Notification(name: notificationImage))
+        }
+       
+    }
+    
+    
+    static func updatePost(data: PostStr?){
+       // print("I am UseCase, I got new information from Repository, I will process it and update View")
         var post:Post
         if let data = data{
             post = Post(author: data.author, domain: data.domain, created: Date(timeIntervalSince1970: Double(data.created) ?? 0.0), title: data.title, numComments: Int(data.numComments) ?? 0, ups: Int(data.ups) ?? 0, downs: Int(data.downs) ?? 0, thumbnail: data.thumbnail)
@@ -25,7 +44,8 @@ class UseCase{
             post = Post()
         }
         self.data = post
-        NotificationCenter.default.post(Notification(name: notificationName))
+        NotificationCenter.default.post(Notification(name: notificationPost))
+        requestForImage()
     }
 }
 
